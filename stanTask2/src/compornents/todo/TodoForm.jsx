77ef6c076e9c208e3d8341cf06a3720a3fb4b0be
YIS <dtdt203@naver.com>
2024-05-19 18:07:53 +0900

    TodoForm 완료

--- stanTask2/src/compornents/todo/TodoForm.jsx
+++ stanTask2/src/compornents/todo/TodoForm.jsx
@@ -1,7 +1,46 @@
 import { useState } from "react";
 
-const TodoForm = () => {
-  return <div>TodoForm</div>;
+const TodoForm = ({ setTodos }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (!title.trim() || !content.trim()) {
+      return alert("제목과 내용을 입력하세요");
+    }
+    const newCard = {
+      id: crypto.randomUUID(),
+      title,
+      content,
+      isDone: false,
+    };
+
+    setTodos((prev) => [...prev, newCard]);
+    setTitle("");
+    setContent("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={title}
+          placeholder="제목"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          value={content}
+          placeholder="내용"
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <button type="submit">추가</button>
+      </form>
+    </div>
+  );
 };
 
 export default TodoForm;
