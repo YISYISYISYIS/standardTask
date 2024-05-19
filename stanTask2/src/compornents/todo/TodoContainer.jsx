b3dd34b7bc83b49f54e653bd816f4c6e68853d92
YIS <dtdt203@naver.com>
2024-05-19 17:45:59 +0900

    TodoContainer 완료

--- stanTask2/src/compornents/todo/TodoContainer.jsx
+++ stanTask2/src/compornents/todo/TodoContainer.jsx
@@ -3,11 +3,30 @@ import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
 
 const TodoContainer = () => {
+  const defaultCard = [
+    {
+      id: 1,
+      title: "리액트 공부",
+      content: "리액트 공부를 합니다.",
+      isDone: false,
+    },
+    {
+      id: 2,
+      title: "리액트 공부 두번째",
+      content: "리액트 공부를 계속 합니다!!.",
+      isDone: true,
+    },
+  ];
+  const [todos, setTodos] = useState(defaultCard);
+
+  const WorkingTodos = todos.filter((todo) => !todo.isDone);
+  const DoneTodos = todos.filter((todo) => todo.isDone);
+
   return (
     <div>
-      <TodoForm />
-      <TodoList />
-      <TodoList />
+      <TodoForm setTodos={setTodos} />
+      <TodoList setTodos={setTodos} title="Working" todo={WorkingTodos} />
+      <TodoList setTodos={setTodos} title="Done" todo={DoneTodos} />
     </div>
   );
 };
