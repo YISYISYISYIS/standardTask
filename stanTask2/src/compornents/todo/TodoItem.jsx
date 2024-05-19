e860c8b43b5b9de493c8bcda79dae89cdd818925
YIS <dtdt203@naver.com>
2024-05-19 18:48:27 +0900

    Todoitem 완료

--- stanTask2/src/compornents/todo/TodoItem.jsx
+++ stanTask2/src/compornents/todo/TodoItem.jsx
@@ -1,5 +1,27 @@
-const TodoItem = () => {
-  return <div>TodoItem</div>;
+const TodoItem = ({ setTodos, todo }) => {
+  const { id, title, content, isDone } = todo;
+  const deleteTodo = () => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const toggleTodo = () => {
+    setTodos((prev) =>
+      prev.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+  return (
+    <div>
+      {title}
+      {content}
+
+      <div>
+        <button onClick={toggleTodo}>{!isDone ? "확인" : "취소"}</button>
+        <button onClick={deleteTodo}>삭제</button>
+      </div>
+    </div>
+  );
 };
 
 export default TodoItem;
